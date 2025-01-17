f44604f90bc953a4da9f9b699dfb8501a73676a4
Nabaraj saha <nabaraj2k@gmail.com>
2025-01-18 00:21:59 +0530

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,13 @@ import EditorForm from "./components/EditorForm.vue";
 </script>
 
 <template>
-  <main class="fixed w-full h-full overflow-auto">
+  <main class="main fixed w-full h-full overflow-auto">
     <EditorForm />
   </main>
 </template>
 
 <style scoped>
-
+.main{
+  background-image: url('https://images.unsplash.com/photo-1529587137368-dec9bab48c56?q=80&w=3570&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
+}
 </style>
